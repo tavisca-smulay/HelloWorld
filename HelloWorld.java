e90166200d4c702af05735d7de4693a456e131bd
tavisca-smulay <smulay@tavisca.in>
2019-06-20 01:57:53 +0530

    With Arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
+	Scanner.sc = new Scanner(System.in);
+	int a = sc.nextInt();
     } 
 } 
